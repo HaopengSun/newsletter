df185a7888f63c134f36ee5ffa3992e6c99124d0
HaopengSun <sunhaopeng2016@gmail.com>
2021-09-15 11:04:24 -0400

    arrow for slides

--- src/conponents/card/Card.jsx
+++ src/conponents/card/Card.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import "./Card.scss";
 
 export default function Card(props) {
-  return <div className="card">
-    <div className="title">{props.title}</div>
-    <p className="content">{props.content}</p>
+  return <div className="container">
+    <div className="item">
+      <div className="left">
+        <div className="leftContainer">
+          <div className="card">
+            <div className="title">{props.title}</div>
+            <p className="content">{props.content}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 }
--- src/conponents/content/Content.jsx
+++ src/conponents/content/Content.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Card from "../card/Card";
 import axios from 'axios';
-
+import arrow from './arrow.png'
 import "./Content.scss";
 
 export default function Content() {
   const [services, setServices] = useState([])
+  const [currentSlide, setCurrentSlide] = useState(0)
 
   useEffect(() => {
     axios.get('./data.json').then(response => {
       setServices(response.data)
     })
   }, [])
+
+  const nextSlide = function(next){
+    next === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3)
+    : setCurrentSlide(currentSlide < 3 ? currentSlide + 1 : 0)
+  }
 
   return (
     <div className="content">
@@ -24,7 +30,15 @@ export default function Content() {
         <br/>
       </div>
       <div className="down">
-        {services.map((service, idx) => <Card key={idx} title={service.title} content={service.content}/>)}
+        <div className="service" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+          {services.map((service, idx) => 
+            <Card key={idx} title={service.title} content={service.content}/>
+          )}
+        </div>
+      </div>
+      <div className="arrows">
+        <img src={arrow} className="arrow left" alt="arrow" onClick={() => nextSlide("left")}/>
+        <img src={arrow} className="arrow right" alt="arrow" onClick={() => nextSlide()}/>
       </div>
     </div>
   )
